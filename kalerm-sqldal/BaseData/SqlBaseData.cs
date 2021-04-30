a5946ea90862571c2679698f15eb3b58ebb01902
nizeheng <nizh@m3lean.com>
2021-04-30 11:57:22 +0800

    封装方法。

--- kalerm-sqldal/BaseData/SqlBaseData.cs
+++ kalerm-sqldal/BaseData/SqlBaseData.cs
@@ -24,18 +24,10 @@ namespace kalerm_sqldal.BaseData
             string sql = string.Format(@"select a.CustomerCode as Code,a.CustomerName as Name from sys_customer a left join `kalerm-app-aps`.`worksheet` b on a.CustomerCode=b.CustomerCode");
             try
             {
-                DataSet ds = new DataSet();
-                using (DbConnection dbConnection = base.Context.CreateConnection(ConnectionType.model))
-                {
-                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
-                    sqlCommand.CommandText = sql;
-                    MySqlDataAdapter da = new MySqlDataAdapter(sqlCommand);
-                    da.Fill(ds);
-                    sqlCommand.Dispose();
-                    dbConnection.Close();
-                }
-                if (ds != null && ds.Tables[0].Rows.Count > 0)
-                    list = Common.DataTableConvertList<ReportBaseModel>(ds.Tables[0]);
+                DataTable dt = new DataTable();
+                dt = GetDataTable(sql, ConnectionType.model);
+                if (dt != null && dt.Rows.Count > 0)
+                    list = Common.DataTableConvertList<ReportBaseModel>(dt);
                 return list;
             }
             catch (Exception ex)
@@ -43,5 +35,269 @@ namespace kalerm_sqldal.BaseData
                 throw new Exception(ex.Message);
             }
         }
+
+        #region sql语句
+
+        /// <summary>
+        /// 返回DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public DataTable GetDataTable(string sql, ConnectionType type)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    sqlCommand.CommandText = sql;
+                    MySqlDataAdapter da = new MySqlDataAdapter(sqlCommand);
+                    da.Fill(dt);
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 返回DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public DataTable GetDataTable(string sql, ConnectionType type, params MySqlParameter[] pms)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    sqlCommand.CommandText = sql;
+                    if (pms != null)
+                    {
+                        foreach (MySqlParameter item in pms)
+                        {
+                            if (item != null)
+                            {
+                                sqlCommand.Parameters.Add(item);
+                            }
+                        }
+                    }
+                    MySqlDataAdapter da = new MySqlDataAdapter(sqlCommand);
+                    da.Fill(dt);
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 执行sql
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string sql, ConnectionType type)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    int i = sqlCommand.ExecuteNonQuery();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return i;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }  
+        }
+
+        /// <summary>
+        /// 执行sql
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string sql, ConnectionType type, params MySqlParameter[] pms)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    if (pms != null)
+                    {
+                        foreach (MySqlParameter item in pms)
+                        {
+                            if (item != null)
+                            {
+                                sqlCommand.Parameters.Add(item);
+                            }
+                        }
+                    }
+                    int i = sqlCommand.ExecuteNonQuery();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return i;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 返回首行首列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public object ExecuteScalar(string sql, ConnectionType type)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    object i = sqlCommand.ExecuteScalar();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return i;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 返回首行首列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public object ExecuteScalar(string sql, ConnectionType type, params MySqlParameter[] pms)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    if (pms != null)
+                    {
+                        foreach (MySqlParameter item in pms)
+                        {
+                            if (item != null)
+                            {
+                                sqlCommand.Parameters.Add(item);
+                            }
+                        }
+                    }
+                    object i = sqlCommand.ExecuteScalar();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return i;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 返回游标对象
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public MySqlDataReader ExecuteReader(string sql, ConnectionType type)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    MySqlDataReader sdr = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                    sdr.Close();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return sdr;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 返回游标对象
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="type"></param>
+        /// <param name="pms"></param>
+        /// <returns></returns>
+        public MySqlDataReader ExecuteReader(string sql, ConnectionType type, params MySqlParameter[] pms)
+        {
+            try
+            {
+                using (DbConnection dbConnection = base.Context.CreateConnection(type))
+                {
+                    //dbConnection.Open();
+                    MySqlCommand sqlCommand = (MySqlCommand)base.Context.CreateCommand(sql, dbConnection);
+                    if (pms != null)
+                    {
+                        foreach (MySqlParameter item in pms)
+                        {
+                            if (item != null)
+                            {
+                                sqlCommand.Parameters.Add(item);
+                            }
+                        }
+                    }
+                    MySqlDataReader sdr = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                    sdr.Close();
+                    sqlCommand.Dispose();
+                    dbConnection.Close();
+                    return sdr;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
     }
 }
